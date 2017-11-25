c62ad94ca6c225dc89d11f9e914fe68af3bcc394
Koji Nakagawa <lightupthesky0627@gmail.com>
2017-11-25 12:38:15 -0800

    finish capitalize

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,52 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+
+function capitalize(str) {
+
+  //SOLUTION 2
+  let result = str[0].toUpperCase();
+
+  for (let i= 1; i < str.length; i++) {
+    if (str[i - 1] === ' ') {
+      result += str[i].toUpperCase();
+    } else {
+      result += str[i];
+    }
+  }
+  return result;
+}
+
+// SOLUTION 1 BETTER SOLUTION
+// function capitalize(str) {
+//   const words = [];
+//
+//   for (let word of str.split(' ')) {
+//     words.push(word[0].toUpperCase() + word.slice(1));
+//   }
+//   return  words.join(' ');
+// }
+
+
+
+// MY SOLUTION
+// function capitalize(str) {
+//   let wordArr = str.split(" ");
+//
+//   for (let i=0; i<wordArr.length; i++) {
+//       wordArr[i] = capitalizeFirstLetter(wordArr[i]);
+//
+//   }
+//
+//   return wordArr.join(" ");
+// }
+//
+//
+// function capitalizeFirstLetter(str) {
+//     const firstLetter = str[0].toUpperCase();
+//     const restOfLetter = str.slice(1);
+//     const letter = firstLetter + restOfLetter;
+//     return letter;
+// }
 
 module.exports = capitalize;
