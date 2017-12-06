4ce1c127743b3997b105a6edbdbe706a45a66a9e
Koji Nakagawa <lightupthesky0627@gmail.com>
2017-12-06 14:28:51 -0800

    worked on steps

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,40 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+function steps(n, row = 0, stair = "") {
+// Solution 2 (Recursive)
+
+  if (n === row) {
+      return;
+  }
+
+  if (n === stair.length) {
+    console.log(stair);
+    steps(n, row + 1);
+    return;
+  }
+
+  const add = stair.length <= row ? '#' : ' ';
+
+  steps(n, row, stair + add);
+
+}
 
 module.exports = steps;
+
+
+// //SOLUTION 1
+//
+// for (let row = 0; row < n; row++) {
+//
+//   let step = "";
+//
+//   for (let col = 0; col < n; col++) {
+//     if (col <= row) {
+//       step += "#";
+//     } else {
+//       step += " ";
+//     }
+//   }
+//   console.log(step);
+// }
