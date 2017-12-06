a6fdb5c7bce5eb9d14c0be57946bf1df5f4512cb
Koji Nakagawa <lightupthesky0627@gmail.com>
2017-12-06 15:37:32 -0800

    finish pyramid

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -5,15 +5,73 @@
 // pyramid has spaces on both the left *and* right hand sides
 // --- Examples
 //   pyramid(1)
-//       '#'
+//       '#' 1
 //   pyramid(2)
 //       ' # '
-//       '###'
+//       '###' 3
 //   pyramid(3)
-//       '  #  '
-//       ' ### '
-//       '#####'
+//       '  #  ' 1 , 2
+//       ' ### ' 2 , 1,2,3
+//       '#####' 3 , 0,1,2,3,4
+//   pyramid(4)
+//      '   #   '1, 3
+//      '  ###  '2, 2,3,4
+//      ' ##### '3, 1,2,3,4,5
+//      '#######'4, 0,1,2,3,4,5,6
 
-function pyramid(n) {}
+function pyramid(n) {
+
+  // Solution3
+  let str, pad;
+  for (let i = 0; i < n; i++) {
+    str = '#'.repeat(i * 2 + 1);
+    pad = ' '.repeat(n - i - 1);
+    console.log(pad + str + pad);
+  }
+
+
+}
 
 module.exports = pyramid;
+
+// //SOLUTION 1
+// const midpoint = Math.floor((2 * n - 1) / 2);
+//
+// for (let row = 0; row < n; row++) {
+//   let level = '';
+//
+//   for (let column = 0; column < 2 * n - 1; column++) {
+//       if (midpoint - row  <= column && midpoint + row >= column) {
+//         level += '#';
+//       } else {
+//         level += ' ';
+//       }
+//
+//   }
+//   console.log(level);
+// }
+
+// SOLUTION 2 Recursive
+
+// function pyramid(n, row = 0, level = '') {
+//
+//   if (row === n) {
+//     return;
+//   }
+//
+//   if (level.length === 2 * n - 1) {
+//     console.log(level);
+//     return pyramid(n, row + 1);
+//   }
+//
+//   const midpoint = Math.floor((2 * n - 1) / 2);
+//   let add;
+//
+//   if (midpoint - row <= level.length && midpoint + row >= level.length) {
+//     add = '#';
+//   } else {
+//     add = ' ';
+//   }
+//
+//   pyramid(n, row, level + add);
+// }
